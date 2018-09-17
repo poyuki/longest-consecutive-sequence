1dec0b05cdf6d40c1cbd3e90f616d7ed538f4222
poyuKiryl <poyuki9@gmail.com>
2018-09-17 15:59:41 +0300

    Find long sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    if (!array.length) return 0;
+    let set = new Set();
+    array.forEach((val) => {
+        set.add(val);
+    });
+    let m=0;
+    array.forEach((value)=>{
+        let previous=value-1,
+            next=value+1,
+            count=1;
+        while (set.has(previous)){
+            set.delete(previous);
+            previous--;
+            count++;
+        }
+        while (set.has(next)){
+            set.delete(next);
+            next++;
+            count++;
+        }
+        m=count>m?count:m;
+    });
+    return m
+};
